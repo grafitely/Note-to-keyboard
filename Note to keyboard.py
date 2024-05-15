41fe6ca90f771b62ef2f52b4a05a203397dc451c
Oliver <88990666+grafitely@users.noreply.github.com>
2024-05-15 01:45:03 +0100

    Add files via upload
    added comments and removed unecessary code

--- Note to keyboard.py
+++ Note to keyboard.py
@@ -1,25 +1,29 @@
+#import modules
 import pyaudio
 import numpy as np
 import pyautogui
 import time
 import threading
 
+#define the pyaudio config
 Rate = 48000
 Chunk = 2048
 Inputformat = pyaudio.paInt16
 
+#initialise pyaudio and open audio stream
 p = pyaudio.PyAudio()
-space = "space"
-
 stream = p.open(format=Inputformat, rate=Rate, channels=1, input=True, frames_per_buffer=Chunk)
 
+#presses a key with pyautogui
 def pressKey(key):
     pyautogui.keyDown(key)
     time.sleep(0.6)
     pyautogui.keyUp(key)
 
+#main function
 def audio():
     while stream.is_active:
+        #grabs data from active audio stream and runs it through a fourier transform
         data = np.frombuffer(stream.read(Chunk), dtype=np.int16)
         fftData=abs(np.fft.rfft(data))**2
         which = fftData[1:].argmax() + 1
@@ -33,10 +37,13 @@ def audio():
         else:
             thefreq = which*Rate/Chunk
             print("The freq is " + str(round(thefreq)) + " Hz.")
+        
+        #rounds the frequency for use in keyboard output
         roundedfreq = round(thefreq) 
 
         match roundedfreq:
             case roundedfreq if roundedfreq < 530 and roundedfreq > 490:
+                #starts thread to send keyboard output with
                 thread_two = threading.Thread(target=pressKey, args="d")
                 thread_two.start()
             case roundedfreq if roundedfreq < 560 and roundedfreq > 530:
@@ -51,8 +58,6 @@ def audio():
             case roundedfreq if roundedfreq < 740 and roundedfreq > 710:
                 pyautogui.leftClick()
 
+#initialise thread for the frequency grabbing
 thread_one = threading.Thread(target=audio)
-
 thread_one.start()
-
-audio()
